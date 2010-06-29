4a6286c35197def432d2d4e00d11fc95214e8fc0
lockner <>
2010-06-29 16:09:14 +0000

    rewighting events using PDF weights

---
+++ src/analysisClass_PDFweighting.C
@@ -0,0 +1,408 @@
+#define analysisClass_cxx
+#include "analysisClass.h"
+#include <TH2.h>
+#include <TH1F.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TLorentzVector.h>
+#include <TVector2.h>
+#include <TVector3.h>
+
+
+
+
+//-----------------------------
+//### JetID ### --> see https://twiki.cern.ch/twiki/bin/view/CMS/ExoticaHighPtJets#JetId
+
+bool JetIdloose(double ak5JetJIDresEMF,double ak5JetJIDfHPD,int ak5JetJIDn90Hits, double ak5JetEta){
+  bool jetidloose=false;
+  bool jetidresEMF=true;
+
+  double fhpdmax = 0.98;
+  double n90hitsmin =1;
+  double emf_min = 0.01;
+
+  if(fabs(ak5JetEta)<2.6 && ak5JetJIDresEMF<=emf_min) jetidresEMF=false;
+
+  if(jetidresEMF && ak5JetJIDfHPD<fhpdmax && ak5JetJIDn90Hits>n90hitsmin) {
+    jetidloose=true;
+  }
+  return jetidloose;
+}
+
+bool JetIdtight(double ak5JetJIDresEMF,double ak5JetJIDfHPD,int ak5JetJIDn90Hits, double ak5JetEta, double ak5JetPtRaw){
+  bool jetidtight=false;
+  bool jetidresEMF=true;
+  bool jetidfHPD_highPt=true;
+
+  double fhpdmax = 0.98;
+  double n90hitsmin =1;
+  double emf_min = 0.01;
+
+  if(fabs(ak5JetEta)<2.6 && ak5JetJIDresEMF<=emf_min) jetidresEMF=false;
+  if(fabs(ak5JetEta)<2.6 && ak5JetPtRaw>80 && ak5JetJIDresEMF>=1) jetidresEMF=false;
+  if(ak5JetPtRaw>25 && ak5JetJIDfHPD>=0.95) jetidfHPD_highPt=false;
+
+  if(jetidresEMF && jetidfHPD_highPt && ak5JetJIDfHPD<fhpdmax && ak5JetJIDn90Hits>n90hitsmin) 
+    {
+      jetidtight=true;
+    }
+  return jetidtight;
+}
+
+//-----------------------------
+
+analysisClass::analysisClass(string * inputList, string * cutFile, string * treeName, string * outputFileName, string * cutEfficFile)
+  :baseClass(inputList, cutFile, treeName, outputFileName, cutEfficFile)
+{
+  //STDOUT("analysisClass::analysisClass() was called");
+}
+
+analysisClass::~analysisClass()
+{
+  //STDOUT("analysisClass::~analysisClass() was called");
+}
+
+void analysisClass::Loop()
+{
+  //STDOUT("analysisClass::Loop() begins");
+  
+  if (fChain == 0) return;
+   
+  ////////////////////// User's code to book histos - BEGIN ///////////////////////
+
+  TH1F *h_Mej = new TH1F ("h_Mej","h_Mej",200,0,2000);  h_Mej->Sumw2();
+
+  TH1F *h_PDFWeight_eff = new TH1F ("PDFWeight_eff","PDFWeight_eff",41,-0.5,40.5); h_PDFWeight_eff->Sumw2();
+  TH1F *h_PDFWeight_Npass = new TH1F ("PDFWeight_Npass","PDFWeight_Npass",41,-0.5,40.5); h_PDFWeight_Npass->Sumw2();
+
+  double cum_weights[41];
+  double cum_weights_PASS[41];
+  for (int i=0;i<41;i++) {
+    cum_weights[i]=0;
+    cum_weights_PASS[i]=0;
+  }
+  
+  ////////////////////// User's code to book histos - END ///////////////////////
+    
+  Long64_t nentries = fChain->GetEntriesFast();
+  STDOUT("analysisClass::Loop(): nentries = " << nentries);   
+  
+  ////// The following ~7 lines have been taken from rootNtupleClass->Loop() /////
+  ////// If the root version is updated and rootNtupleClass regenerated,     /////
+  ////// these lines may need to be updated.                                 /////    
+  Long64_t nbytes = 0, nb = 0;
+  for (Long64_t jentry=0; jentry<nentries;jentry++) { // Begin of loop over events
+  //for (Long64_t jentry=0; jentry<115;jentry++) { // Begin of loop over events
+    Long64_t ientry = LoadTree(jentry);
+    if (ientry < 0) break;
+    nb = fChain->GetEntry(jentry);   nbytes += nb;
+    if(jentry < 10 || jentry%1000 == 0) STDOUT("analysisClass::Loop(): jentry = " << jentry);   
+    // if (Cut(ientry) < 0) continue;
+    
+    ////////////////////// User's code to be done for every event - BEGIN ///////////////////////
+
+    // PDF weight
+    double CentralWeight = PDFWeights[0][0] ;
+    for(int pdf=0;pdf<PDFWeights[0].size();pdf++){
+      double tmp = (PDFWeights[0][pdf]/CentralWeight);
+      if (tmp!=tmp) continue; // solves nan problem
+      cum_weights[pdf]=cum_weights[pdf]+tmp;
+    }
+
+    //## HLT
+    bool PassTrig=HLTResults->at(1); // results of HLTPhoton15 
+
+    // Electrons
+    vector<int> v_idx_ele_all;
+    vector<int> v_idx_ele_PtCut;
+    vector<int> v_idx_ele_PtCut_IDISO_noOverlap;
+    int eleIDType = (int) getPreCutValue1("eleIDType");
+     
+    //Loop over electrons
+    for(int iele=0; iele<ElectronPt->size(); iele++)
+      {
+	//no cut on reco electrons
+	v_idx_ele_all.push_back(iele); 
+
+	//pT pre-cut on ele
+	if( ElectronPt->at(iele) < getPreCutValue1("ele_PtCut") ) continue; 
+	v_idx_ele_PtCut.push_back(iele);
+
+	//ID + ISO + NO overlap with good muons 
+	int eleID = ElectronPassID->at(iele);
+	if ( (eleID & 1<<eleIDType) > 0  && ElectronOverlaps->at(iele)==0 )
+	  {
+	    v_idx_ele_PtCut_IDISO_noOverlap.push_back(iele);
+	  }
+
+      } // End loop over electrons
+
+    
+    // Jets
+    vector<int> v_idx_jet_all;
+    vector<int> v_idx_jet_PtCut;
+    vector<int> v_idx_jet_PtCut_noOverlap;
+    vector<int> v_idx_jet_PtCut_noOverlap_ID;
+
+    // Loop over jets
+    for(int ijet=0; ijet<CaloJetPt->size(); ijet++)
+      {
+
+	//no cut on reco jets
+	v_idx_jet_all.push_back(ijet);
+
+	//pT pre-cut on reco jets
+	if ( CaloJetPt->at(ijet) < getPreCutValue1("jet_PtCut") ) continue;
+
+	v_idx_jet_PtCut.push_back(ijet);
+
+	//pT pre-cut + no overlaps with electrons
+	if( ( CaloJetOverlaps->at(ijet) & 1 << eleIDType) == 0)/* NO overlap with electrons */  
+	  // && (caloJetOverlaps[ijet] & 1 << 5)==0 )/* NO overlap with muons */   
+	  v_idx_jet_PtCut_noOverlap.push_back(ijet);
+
+	//pT pre-cut + no overlaps with electrons + jetID
+	bool passjetID = JetIdloose(CaloJetresEMF->at(ijet),CaloJetfHPD->at(ijet),CaloJetn90Hits->at(ijet), CaloJetEta->at(ijet));
+	if( (CaloJetOverlaps->at(ijet) & 1 << eleIDType) == 0  /* NO overlap with electrons */  
+	    && passjetID == true )                                 /* pass JetID */
+	    // && (caloJetOverlaps[ijet] & 1 << 5)==0 )         /* NO overlap with muons */      
+	  v_idx_jet_PtCut_noOverlap_ID.push_back(ijet);
+
+	//NOTE: We should verify that caloJetOverlaps match with the code above
+      } // End loop over jets
+
+
+    //## Muons
+    vector<int> v_idx_muon_all;
+    vector<int> v_idx_muon_PtCut;
+    vector<int> v_idx_muon_PtCut_IDISO;
+
+    // Loop over muons
+    for(int imuon=0; imuon<MuonPt->size(); imuon++)
+      {
+	//no cut on reco muons
+	v_idx_muon_all.push_back(imuon);
+
+	//pT pre-cut on ele
+	if ( MuonPt->at(imuon) < getPreCutValue1("muon_PtCut") ) continue;
+	 
+	v_idx_muon_PtCut.push_back(imuon);
+
+	//ID + ISO
+	if ( MuonPassIso->at(imuon)==1 && MuonPassID->at(imuon)==1)
+	  v_idx_muon_PtCut_IDISO.push_back(imuon);
+
+      } // End loop over muons
+
+
+    // Set the evaluation of the cuts to false and clear the variable values and filled status
+    resetCuts();
+    
+
+    // Set the value of the variableNames listed in the cutFile to their current value
+    
+    // Trigger (L1 and HLT)
+    if(isData==true)
+      {
+	fillVariableWithValue( "PassBPTX0", isBPTX0 ) ;
+	fillVariableWithValue( "PassPhysDecl", isPhysDeclared ) ;       
+      }
+    else
+      {
+	fillVariableWithValue( "PassBPTX0", true ) ;
+	fillVariableWithValue( "PassPhysDecl", true ) ;       
+      }
+
+    //fillVariableWithValue( "PassHLT", PassTrig ) ;
+
+    //Event filters at RECO level
+    fillVariableWithValue( "PassBeamScraping", !isBeamScraping ) ;
+    fillVariableWithValue( "PassPrimaryVertex", isPrimaryVertex ) ;
+    fillVariableWithValue( "PassHBHENoiseFilter", passLooseNoiseFilter ) ;
+
+    // nEle
+    fillVariableWithValue( "nEle_all", v_idx_ele_all.size() ) ;
+    fillVariableWithValue( "nEle_PtCut", v_idx_ele_PtCut.size() ) ;
+    fillVariableWithValue( "nEle_PtCut_IDISO_noOvrlp", v_idx_ele_PtCut_IDISO_noOverlap.size() ) ;
+     
+    // nJet
+    fillVariableWithValue( "nJet_all", v_idx_jet_all.size() ) ;
+    fillVariableWithValue( "nJet_PtCut", v_idx_jet_PtCut.size() ) ;
+    fillVariableWithValue( "nJet_PtCut_noOvrlp", v_idx_jet_PtCut_noOverlap.size() ) ;
+    fillVariableWithValue( "nJet_PtCut_noOvrlp_ID", v_idx_jet_PtCut_noOverlap_ID.size() ) ;
+
+    // 1st ele
+    if( v_idx_ele_PtCut_IDISO_noOverlap.size() >= 1 ) 
+      {
+	fillVariableWithValue( "Pt1stEle_IDISO_NoOvrlp", ElectronPt->at(v_idx_ele_PtCut_IDISO_noOverlap[0]) );
+	fillVariableWithValue( "Eta1stEle_IDISO_NoOvrlp", ElectronEta->at(v_idx_ele_PtCut_IDISO_noOverlap[0]) );
+	fillVariableWithValue( "mEta1stEle_IDISO_NoOvrlp", fabs(ElectronEta->at(v_idx_ele_PtCut_IDISO_noOverlap[0])) );
+      }
+
+    // 2nd ele
+    if( v_idx_ele_PtCut_IDISO_noOverlap.size() >= 2 ) 
+      {
+	fillVariableWithValue( "Pt2ndEle_IDISO_NoOvrlp", ElectronPt->at(v_idx_ele_PtCut_IDISO_noOverlap[1]) );
+	fillVariableWithValue( "Eta2ndEle_IDISO_NoOvrlp", ElectronEta->at(v_idx_ele_PtCut_IDISO_noOverlap[1]) );
+	fillVariableWithValue( "mEta2ndEle_IDISO_NoOvrlp", fabs(ElectronEta->at(v_idx_ele_PtCut_IDISO_noOverlap[1])) );
+	fillVariableWithValue( "maxMEtaEles_IDISO_NoOvrl", max( getVariableValue("mEta1stEle_IDISO_NoOvrlp"), getVariableValue("mEta2ndEle_IDISO_NoOvrlp") ) );
+      }
+
+    // 1st jet
+    if( v_idx_jet_PtCut_noOverlap_ID.size() >= 1 ) 
+      {
+	fillVariableWithValue( "Pt1stJet_noOvrlp_ID", CaloJetPt->at(v_idx_jet_PtCut_noOverlap_ID[0]) );
+	fillVariableWithValue( "Eta1stJet_noOvrlp_ID", CaloJetEta->at(v_idx_jet_PtCut_noOverlap_ID[0]) );
+	fillVariableWithValue( "mEta1stJet_noOvrlp_ID", fabs(CaloJetEta->at(v_idx_jet_PtCut_noOverlap_ID[0])) );
+      }
+
+
+    //cout << "2nd Jet" << endl;
+    //## 2nd jet
+    if( v_idx_jet_PtCut_noOverlap_ID.size() >= 2 ) 
+      {
+	fillVariableWithValue( "Pt2ndJet_noOvrlp_ID", CaloJetPt->at(v_idx_jet_PtCut_noOverlap_ID[1]) );
+	fillVariableWithValue( "Eta2ndJet_noOvrlp_ID", CaloJetEta->at(v_idx_jet_PtCut_noOverlap_ID[1]) );
+	fillVariableWithValue( "mEta2ndJet_noOvrlp_ID", fabs(CaloJetEta->at(v_idx_jet_PtCut_noOverlap_ID[1])) );
+	fillVariableWithValue( "maxMEtaJets_noOvrlp_ID", max( getVariableValue("mEta1stJet_noOvrlp_ID"), getVariableValue("mEta2ndJet_noOvrlp_ID") ) );
+      }
+
+    //## define "2ele" and "2jets" booleans
+    bool TwoEle=false;
+    bool TwoJets=false;
+    if( v_idx_ele_PtCut_IDISO_noOverlap.size() >= 2 ) TwoEle = true;
+    if( v_idx_jet_PtCut_noOverlap_ID.size() >= 2 ) TwoJets = true;
+
+    // ST
+    double calc_sT=-999.; 
+    if ( (TwoEle) && (TwoJets) ) 
+      {
+	calc_sT = 
+	  ElectronPt->at(v_idx_ele_PtCut_IDISO_noOverlap[0]) +
+	  ElectronPt->at(v_idx_ele_PtCut_IDISO_noOverlap[1]) +
+	  CaloJetPt->at(v_idx_jet_PtCut_noOverlap_ID[0]) +
+	  CaloJetPt->at(v_idx_jet_PtCut_noOverlap_ID[1]);
+	fillVariableWithValue("sT", calc_sT);
+      }
+
+    // Mee
+    if (TwoEle)
+      {
+	TLorentzVector ele1, ele2, ee;
+	ele1.SetPtEtaPhiM(ElectronPt->at(v_idx_ele_PtCut_IDISO_noOverlap[0]),
+			  ElectronEta->at(v_idx_ele_PtCut_IDISO_noOverlap[0]),
+			  ElectronPhi->at(v_idx_ele_PtCut_IDISO_noOverlap[0]),0);
+	ele2.SetPtEtaPhiM(ElectronPt->at(v_idx_ele_PtCut_IDISO_noOverlap[1]),
+			  ElectronEta->at(v_idx_ele_PtCut_IDISO_noOverlap[1]),
+			  ElectronPhi->at(v_idx_ele_PtCut_IDISO_noOverlap[1]),0);
+	ee = ele1+ele2;
+	fillVariableWithValue("Mee", ee.M());
+
+
+
+	if(isData==true) 
+	  {
+	    STDOUT("Two electrons: Run, LS, Event = "<<run<<", "<<ls<<", "<<event);
+	    STDOUT("Two electrons: M_ee, Pt_ee, Eta_ee, Phi_ee = "<<ee.M() <<", "<< ee.Pt() <<", "<< ee.Eta() <<", "<< ee.Phi());
+	    STDOUT("Two electrons: 1st ele Pt, eta, phi = "<< ele1.Pt() <<", "<< ele1.Eta() <<", "<< ele1.Phi() );
+	    STDOUT("Two electrons: 2nd ele Pt, eta, phi = "<< ele2.Pt() <<", "<< ele2.Eta() <<", "<< ele2.Phi() );
+	  }
+      }
+
+    // Mej 
+    double M11, M12, M21, M22 = -999;
+    double diff_11_22 = 9999;
+    double diff_12_21 = 9999;
+    if ( (TwoEle) && (TwoJets) ) 
+      {
+	TLorentzVector jet1, jet2, ele1, ele2;
+	ele1.SetPtEtaPhiM(ElectronPt->at(v_idx_ele_PtCut_IDISO_noOverlap[0]),
+			  ElectronEta->at(v_idx_ele_PtCut_IDISO_noOverlap[0]),
+			  ElectronPhi->at(v_idx_ele_PtCut_IDISO_noOverlap[0]),0);
+	ele2.SetPtEtaPhiM(ElectronPt->at(v_idx_ele_PtCut_IDISO_noOverlap[1]),
+			  ElectronEta->at(v_idx_ele_PtCut_IDISO_noOverlap[1]),
+			  ElectronPhi->at(v_idx_ele_PtCut_IDISO_noOverlap[1]),0);
+	jet1.SetPtEtaPhiM(CaloJetPt->at(v_idx_jet_PtCut_noOverlap_ID[0]),
+			  CaloJetEta->at(v_idx_jet_PtCut_noOverlap_ID[0]),
+			  CaloJetPhi->at(v_idx_jet_PtCut_noOverlap_ID[0]),0);
+	jet2.SetPtEtaPhiM(CaloJetPt->at(v_idx_jet_PtCut_noOverlap_ID[1]),
+			  CaloJetEta->at(v_idx_jet_PtCut_noOverlap_ID[1]),
+			  CaloJetPhi->at(v_idx_jet_PtCut_noOverlap_ID[1]),0);
+	TLorentzVector jet1ele1, jet2ele1, jet1ele2, jet2ele2;
+	jet1ele1 = jet1 + ele1;
+	jet2ele1 = jet2 + ele1;
+	jet1ele2 = jet1 + ele2;
+	jet2ele2 = jet2 + ele2;
+	M11 = jet1ele1.M();
+	M21 = jet2ele1.M();
+	M12 = jet1ele2.M();
+	M22 = jet2ele2.M();
+
+	diff_11_22 = M11 - M22;
+	diff_12_21 = M12 - M21;
+
+	if(diff_11_22 > diff_12_21)
+	  {
+	    //PAS June 2010
+	    h_Mej->Fill(M12);
+	    h_Mej->Fill(M21);
+	  }
+	else
+	  {
+	    //PAS June 2010
+	    h_Mej->Fill(M22);
+	    h_Mej->Fill(M11);
+	  } 
+      }
+
+
+
+    // Evaluate cuts (but do not apply them)
+    evaluateCuts();
+
+
+
+    if (passedCut("0")&&passedCut("1")){
+      double CentralWeight = PDFWeights[0][0] ;
+      for(int pdf=0;pdf<PDFWeights[0].size();pdf++){
+	cum_weights_PASS[pdf]=cum_weights_PASS[pdf]+(PDFWeights[0][pdf]/CentralWeight);
+	h_PDFWeight_Npass->Fill(pdf,PDFWeights[0][pdf]/CentralWeight);
+      }
+    }
+
+    // Fill histograms and do analysis based on cut evaluation
+    //h_nEleFinal->Fill( ElectronPt->size() );
+     
+
+    //INFO
+    //      // retrieve value of previously filled variables (after making sure that they were filled)
+    //      double totpTEle;
+    //      if ( variableIsFilled("pT1stEle") && variableIsFilled("pT2ndEle") ) 
+    //        totpTEle = getVariableValue("pT1stEle")+getVariableValue("pT2ndEle");
+    //      // reject events that did not pass level 0 cuts
+    //      if( !passedCut("0") ) continue;
+    //      // ......
+    
+     
+    ////////////////////// User's code to be done for every event - END ///////////////////////
+    
+  } // End of loop over events
+  
+
+  ////////////////////// User's code to write histos - BEGIN ///////////////////////
+
+  h_Mej->Write();
+  for (int i=0;i<41;i++) {
+    h_PDFWeight_eff->Fill(i,cum_weights_PASS[i]/cum_weights[i]);
+    }
+  //h_PDFWeight_eff->Divide(h_PDFWeight_PASS,h_PDFWeight,1,1);
+  h_PDFWeight_eff->Write();
+  h_PDFWeight_Npass->Write();
+
+
+  ////////////////////// User's code to write histos - END ///////////////////////
+  
+  
+  //STDOUT("analysisClass::Loop() ends");   
+}
