e8f817fa6569bd78a2aee82f579a598f9fd0c01c
prumerio <>
2010-10-20 10:09:37 +0000

    Mee>110 and added sT cuts

--- src/analysisClass_eejjSample.C
+++ src/analysisClass_eejjSample.C
@@ -573,9 +573,11 @@ void analysisClass::Loop()
 	  CaloJetPt->at(v_idx_jet_PtCut_noOverlap_ID[0]) +
 	  CaloJetPt->at(v_idx_jet_PtCut_noOverlap_ID[1]);
 	fillVariableWithValue("sT", calc_sT);
-	fillVariableWithValue("sT_MLQ200", calc_sT);
 	fillVariableWithValue("sT_MLQ250", calc_sT);
+	fillVariableWithValue("sT_MLQ280", calc_sT);
 	fillVariableWithValue("sT_MLQ300", calc_sT);
+	fillVariableWithValue("sT_MLQ320", calc_sT);
+	fillVariableWithValue("sT_MLQ340", calc_sT);
 	fillVariableWithValue("sT_MLQ400", calc_sT);       
 	//PAS June 2010
 	fillVariableWithValue("sT_PAS", calc_sT);
