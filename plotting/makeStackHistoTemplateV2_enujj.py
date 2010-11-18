4ff46ce16bf1c1bd6a6f76ca7814fcd2a54db1b5
santanas <>
2010-11-18 09:46:33 +0000

    updated part to make .pdf files and copy them in the AN plots directory

--- plotting/makeStackHistoTemplateV2_enujj.py
+++ plotting/makeStackHistoTemplateV2_enujj.py
@@ -1653,14 +1653,14 @@ c.Print("allPlots.ps]")
 
 # Uncomment the 3 lines below to create (not rotated) pdf files,
 # but keep them commented out in cvs since may slow down the execution 
-# print "Converting eps files into pdf files ..."
-# for plot in plots:
-#     system("convert "+plot.name+".eps "+plot.name+".pdf") # instead, uncomment this line to create pdf plots (a bit slow)
+#print "Converting eps files into pdf files ..."
+#for plot in plots:
+#    system("convert "+plot.name+".eps "+plot.name+".pdf") # instead, uncomment this line to create pdf plots (a bit slow)
 
 # create a file with list of eps and pdf files, to facilitate copying them to svn area for AN/PAS/PAPER
 print "Creating file listOfEpsFiles.txt and listOfPdfFiles.txt ..."
 system("rm listOfEpsFiles.txt listOfPdfFiles.txt")
 for plot in plots:
     system("echo "+plot.name+".eps >> listOfEpsFiles.txt; echo "+plot.name+".pdf >> listOfPdfFiles.txt") 
-print "Use for example: scp `cat listOfPdfFiles.txt` pcuscms41:/home/prumerio/doc/cms/phys/leptoquark/ANPAS2010/papers/EXO-10-005/trunk/plots"
+print "Use for example: scp `cat listOfPdfFiles.txt` pcuscms46:/home/santanas/Documents/CMSNotes/notes/AN-10-361/trunk/plots"
 
