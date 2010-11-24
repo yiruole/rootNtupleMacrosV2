a6734a1db141fef9aa07b9477e0d1104522da059
prumerio <>
2010-11-24 18:00:33 +0000

    33.2 pb-1 and more masses

--- plotting/makeFinalNevtTable.sh
+++ plotting/makeFinalNevtTable.sh
@@ -3,14 +3,17 @@
 ##########  USER'S INPUTS BEGIN HERE ############################################################
 
 #FILE=$LQDATA/eejj/10.9pb-1/output_cutTable_eejjSample/analysisClass_eejjSample_tables.dat
-FILE=$LQDATA/eejj/34.7pb-1/output_cutTable_eejjSample_noPreStCut_ZjetsRescaled/analysisClass_eejjSample_tables.dat
+#FILE=$LQDATA/eejj/34.7pb-1/output_cutTable_eejjSample_noPreStCut_ZjetsRescaled/analysisClass_eejjSample_tables.dat
+FILE=$LQDATA/eejj/33.2pb-1/output_cutTable_eejjSample_preSt250_ZjetsRescaled/analysisClass_eejjSample_tables.dat
 
-MASSES="250 280 300 320 340 370 400"
+MASSES="200 250 280 300 320 340 370 400 450 500"
 
 # taken from http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/Leptoquarks/CommonToolsV2/test/exclusion_plot_beta_vs_m_2010Note.C
 # Number and order of the limit values must correspond to number and order of MASSES above !!! 
-xsUp_observed=( 0.364746 0.290527 0.277832 0.268799 0.263184 0.252686 0.244141 )
-xsUp_expected=( 0.424484 0.34855  0.310529 0.285282 0.262771 0.239716 0.219592 )
+# xsUp_observed=( 0.364746 0.290527 0.277832 0.268799 0.263184 0.252686 0.244141 )
+# xsUp_expected=( 0.424484 0.34855  0.310529 0.285282 0.262771 0.239716 0.219592 )
+xsUp_observed=( 0.444824, 0.310547, 0.309082, 0.293945, 0.284668, 0.279297, 0.268066, 0.258789, 0.174243, 0.166113 )
+xsUp_expected=( 0.692479, 0.442443, 0.360792, 0.326146, 0.298866, 0.274453, 0.250794, 0.230144, 0.208843, 0.192884 )
 
 
 
