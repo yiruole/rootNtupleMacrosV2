17478817ab4522c4a7dc90259cb2284d7d677f2c
santanas <>
2010-12-01 14:27:36 +0000

    adding new files with new mass points

--- plotting/makeStackHistoTemplateV2_enujj.py
+++ plotting/makeStackHistoTemplateV2_enujj.py
@@ -337,13 +337,13 @@ class Plot:
 
 #--- Input root file
 
-File_preselection = GetFile("/home/ferencek/work/Leptoquarks/output_fromAFS/enujj_analysis/33.2pb-1_sT_presel_250_Zrescale1.20_Wrescale1.06/analysisClass_enujjSample_plots.root")
+File_preselection = GetFile("/home/santanas/Leptoquarks/data/output_fromAFS/enujj_analysis/33.2pb-1_sT_presel_250_Zrescale1.20_Wrescale1.06/output_cutTable_enujjSample/analysisClass_enujjSample_plots.root")
 
 File_selection    = File_preselection
 
 UseQCDFromData    = 1 # always put an existing file under File_QCD (otherwise the code will crash)
 
-File_QCD          = GetFile("/home/ferencek/work/Leptoquarks/output_fromAFS/enujj_analysis/6.1pb-1_QCD_HLT30_sT_presel_250/analysisClass_enujjSample_QCD_plots.root")
+File_QCD          = GetFile("/home/santanas/Leptoquarks/data/output_fromAFS/enujj_analysis/6.1pb-1_QCD_HLT30_sT_presel_250/output_cutTable_enujjSample_QCD/analysisClass_enujjSample_QCD_plots.root")
 QCDscaleFactor    = 5.4 # 5.4426 # ratio between integrated lumi of the signal
                         # sample (i.e. 33.2 pb-1) / integrated lumi of the QCD sample (i.e. 6.1 pb-1 from HLT Photon30)
 
