ddc2fec109c8ef59f873a58670ab124cd79b7f5a
santanas <>
2010-12-02 11:44:37 +0000

    few fixes in the comments

--- plotting/printEventInfo.py
+++ plotting/printEventInfo.py
@@ -8,10 +8,12 @@ import re
 import pprint # for pretty printing
 import math
 
-#input file(s)
+#==============================================================
+
+#--- input file(s)
 inputFile = "*.log"
 
-#matching string for grep
+#--- matching string for grep
 matchstring_sel = "PassFullSelection"
 #matchstring_sel = "PassMETThreshold"
 #matchstring_sel = "PassMinMETPt1stEleThreshold"
@@ -21,19 +23,21 @@ matchstring_sel = "PassFullSelection"
 #matchstring_sel = "PassMTenuThreshold"
 #matchstring_sel = "PasssTThreshold"
 
-#matching string to create list of run, ls, event number
+#--- matching string to create list of run, ls, event number
 matchstring_run_lS_evNum = "Run"
 
-#exclude columns in this range
+#--- exclude columns in this range
 min = 0
 max = 7
 
-#create file with event details
+#==============================================================
+
+#--- create file with event details
 outputFile = matchstring_sel+"_details.txt"
 os.system("rm -f "+outputFile)
 os.system("grep " + matchstring_sel + " " +  inputFile + " | awk -v f="+str(min)+" -v t="+str(max)+" \'{ for (i=1; i<=NF;i++) if( i>=f && i<=t) continue; else printf(\"%s%s\", $i,(i!=NF) ? OFS : ORS) }\' >> "+outputFile)
 
-#read run, ls, event number --> put it in a dict --> sort it by run+ls
+#--- read run, ls, event number --> put it in a dict --> sort it by run+ls
 d = {}
 for ln, line in enumerate( open(outputFile) ):
     line = line.strip()
@@ -49,7 +53,7 @@ for ln, line in enumerate( open(outputFile) ):
         sum.sort()
 #print sum
 
-#create file with list of events to be used with "edmPickEvents.py"
+#--- create file with list of events to be used with "edmPickEvents.py"
 fout = open(matchstring_sel+"_list.txt", "w")
 for v, value in enumerate(sum):
     #print value    
