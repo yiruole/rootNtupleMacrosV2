83e599446c5fb8d2612cc2d05ef3f7622f383834
santanas <>
2012-01-23 17:00:33 +0000

    save .gif output

--- plotting2011/dev/PhysicsDST2011/HLT_vs_RECO_comparison.C
+++ plotting2011/dev/PhysicsDST2011/HLT_vs_RECO_comparison.C
@@ -2,7 +2,16 @@
   gROOT->Reset();
   gStyle->SetOptFit(1111);
 
-  TFile file("pippoTight.root");
+  //TFile file("HLT_RECO_HT_1kevents_TightJetID.root");
+  //TFile file("HLT_vs_RECO/HTDATA_HLTvsRECO.root"); //DATA (22k events)
+  //TFile file("HLT_vs_RECO/QstarToJJ500MC_HLTvsRECO.root"); //MC (86k events)
+
+  //2011 Selection (only two leading jets, deltaphi cut)
+  //TFile file("HLT_vs_RECO/HTDATA_HLTvsRECO_2011Sel.root"); //DATA (22k events)
+  TFile file("HLT_vs_RECO/QstarToJJ500MC_HLTvsRECO_2011Sel.root"); //MC (86k events)
+  double Case=1;// DATA(0) , MC(1)
+
+  //----------------------------------------------------------------------------------
 
   TH1D *hClone_HLT_M_PFJet1PFJet2 = (TH1D*)file.Get( "h_HLT_M_PFJet1PFJet2" )->Clone(); 
   TH1D *hClone_RECO_M_PFJet1PFJet2 = (TH1D*)file.Get( "h_RECO_M_PFJet1PFJet2" )->Clone(); 
@@ -70,11 +79,21 @@
   legend1.AddEntry(hClone_RECO_M_PFJet1PFJet2,"RECO","pl");
   legend1.Draw();
 
+  if(Case==0)
+    c1.SaveAs("DATA_MJJ_HLT_vs_RECO.gif");
+  else if(Case==1)
+    c1.SaveAs("MC_MJJ_HLT_vs_RECO.gif");
+
   TCanvas c2;
   h2Clone_FinalSelectionAgreement_recoY_vs_hltX->GetXaxis()->SetTitle("Pass HLT selection");
   h2Clone_FinalSelectionAgreement_recoY_vs_hltX->GetYaxis()->SetTitle("Pass RECO selection");
   h2Clone_FinalSelectionAgreement_recoY_vs_hltX->SetMarkerSize(5);
   h2Clone_FinalSelectionAgreement_recoY_vs_hltX->Draw("textcolz");
+
+  if(Case==0)
+    c2.SaveAs("DATA_FinalSelectionAgreement.gif");
+  else if(Case==1)
+    c2.SaveAs("MC_FinalSelectionAgreement.gif");
 
   TCanvas c3;
   hClone_HLT_M_PFJet1PFJet2_MATCH->GetXaxis()->SetTitle("M(Jet1,Jet2) [GeV]");
@@ -91,6 +110,11 @@
   legend3.AddEntry(hClone_RECO_M_PFJet1PFJet2_MATCH,"RECO","pl");
   legend3.Draw();
 
+  if(Case==0)
+    c3.SaveAs("DATA_MJJ_HLT_vs_RECO_MATCH.gif");
+  else if(Case==1)
+    c3.SaveAs("MC_MJJ_HLT_vs_RECO_MATCH.gif");
+
   TCanvas c4;
   TF1 *f_gaus = new TF1("f_gaus","gaus",-0.2,0.2);
   f_gaus->SetLineColor(2);
@@ -99,6 +123,11 @@
   hClone_MassBias_MATCH->SetMarkerStyle(20);
   hClone_MassBias_MATCH->Fit(f_gaus,"R");
   hClone_MassBias_MATCH->Draw();
+
+  if(Case==0)
+    c4.SaveAs("DATA_MJJBias_MATCH.gif");
+  else if(Case==1)
+    c4.SaveAs("MC_MJJBias_MATCH.gif");
 
   TCanvas c5;
   c5->SetGridx();
@@ -113,6 +142,11 @@
   pClone_MassBias_MATCH_vs_Mreco->SetMarkerStyle(20);
   pClone_MassBias_MATCH_vs_Mreco->Fit(f_pol0,"R");
   pClone_MassBias_MATCH_vs_Mreco->Draw();
+
+  if(Case==0)
+    c5.SaveAs("DATA_MJJBiasVsMJJ_MATCH.gif");
+  else if(Case==1)
+    c5.SaveAs("MC_MJJBiasVsMJJ_MATCH.gif");
 
   TCanvas c6;
   hClone_HLT_M_PFJet1PFJet2_MATCH_MassBiasMore0p2->GetXaxis()->SetTitle("M(Jet1,Jet2) [GeV]");
@@ -129,9 +163,10 @@
   legend6.AddEntry(hClone_RECO_M_PFJet1PFJet2_MATCH_MassBiasMore0p2,"RECO","pl");
   legend6.Draw();
 
-  TH1D * = (TH1D*)file.Get( "h_HLT_M_PFJet1PFJet2_HLT1_RECO0" )->Clone(); 
-  TH1D * = (TH1D*)file.Get( "h_RECO_M_PFJet1PFJet2_HLT1_RECO0" )->Clone(); 
-
+  if(Case==0)
+    c6.SaveAs("DATA_MJJBiasVsMJJ_MATCH_MassBiasMore0p2.gif");
+  else if(Case==1)
+    c6.SaveAs("MC_MJJBiasVsMJJ_MATCH_MassBiasMore0p2.gif");
 
   TCanvas c7;
   hClone_HLT_M_PFJet1PFJet2_HLT1_RECO0->GetXaxis()->SetTitle("M(Jet1,Jet2) [GeV]");
@@ -141,12 +176,17 @@
   //hClone_RECO_M_PFJet1PFJet2_HLT1_RECO0->Draw("sames");
   //hClone_HLT_M_PFJet1PFJet2_HLT1_RECO0->GetYaxis()->SetRangeUser(0,TMath::Max( hClone_RECO_M_PFJet1PFJet2_HLT1_RECO0->GetMaximum()*1.2,hClone_HLT_M_PFJet1PFJet2_HLT1_RECO0->GetMaximum()*1.2));
 
-  TLegend legend6 (0.35,0.65,0.75,0.85);
-  legend6.SetFillColor(kWhite);
-  legend6.SetMargin(0.2);
-  legend6.AddEntry(hClone_HLT_M_PFJet1PFJet2_HLT1_RECO0,"HLT","pl");
-  //legend6.AddEntry(hClone_RECO_M_PFJet1PFJet2_HLT1_RECO0,"RECO","pl");
-  legend6.Draw();
+  TLegend legend7 (0.35,0.65,0.75,0.85);
+  legend7.SetFillColor(kWhite);
+  legend7.SetMargin(0.2);
+  legend7.AddEntry(hClone_HLT_M_PFJet1PFJet2_HLT1_RECO0,"HLT","pl");
+  //legend7.AddEntry(hClone_RECO_M_PFJet1PFJet2_HLT1_RECO0,"RECO","pl");
+  legend7.Draw();
+
+  if(Case==0)
+    c7.SaveAs("DATA_MJJ_HLT_HLT1_RECO0.gif");
+  else if(Case==1)
+    c7.SaveAs("MC_MJJ_HLT_HLT1_RECO0.gif");
 
   TCanvas c8;
   c8->SetGridx();
@@ -156,6 +196,11 @@
   hClone_FirstMismatch_HLT1_RECO0->SetFillStyle(3002);
   hClone_FirstMismatch_HLT1_RECO0->Draw("HISTE");
 
+  if(Case==0)
+    c8.SaveAs("DATA_FirstMismatch_HLT1_RECO0.gif");
+  else if(Case==1)
+    c8.SaveAs("MC_FirstMismatch_HLT1_RECO0.gif");
+
   TCanvas c9;
   c9->SetGridx();
   c9->SetGridy();  
@@ -163,6 +208,11 @@
   hClone_FirstMismatch_HLT0_RECO1->SetFillColor(2);
   hClone_FirstMismatch_HLT0_RECO1->SetFillStyle(3002);
   hClone_FirstMismatch_HLT0_RECO1->Draw("HISTE");
+
+  if(Case==0)
+    c9.SaveAs("DATA_FirstMismatch_HLT0_RECO1.gif");
+  else if(Case==1)
+    c9.SaveAs("MC_FirstMismatch_HLT0_RECO1.gif");
 
   TCanvas c10;
   hClone_RECO_M_PFJet1PFJet2_HLT0_RECO1->GetXaxis()->SetTitle("M(Jet1,Jet2) [GeV]");
@@ -177,54 +227,64 @@
   legend10.AddEntry(hClone_RECO_M_PFJet1PFJet2_HLT0_RECO1,"RECO","pl");
   legend10.Draw();
 
-  TCanvas c10;
-  c10.Divide(3,2);
+  if(Case==0)
+    c10.SaveAs("DATA_MJJ_RECO_HLT0_RECO1.gif");
+  else if(Case==1)
+    c10.SaveAs("MC_MJJ_RECO_HLT0_RECO1.gif");
 
-  c10.cd(1);
+  TCanvas c11;
+  c11.Divide(3,2);
+
+  c11.cd(1);
   hClone_HLTPFJetNeutralHadronEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(1);  
   hClone_RECOPFJetNeutralHadronEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(2);  
   hClone_HLTPFJetNeutralHadronEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->Draw();
   hClone_RECOPFJetNeutralHadronEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->Draw("sames");
 
-  c10.cd(2);
+  c11.cd(2);
   hClone_HLTPFJetNeutralEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(1);  
   hClone_RECOPFJetNeutralEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(2);  
   hClone_HLTPFJetNeutralEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->Draw();
   hClone_RECOPFJetNeutralEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->Draw("sames");
 
-  c10.cd(3);
+  c11.cd(3);
   hClone_HLTPFJetNConstituents_smallestDR_Pt30GeV_EtaL2p4->GetXaxis()->SetRangeUser(0,100);
   hClone_HLTPFJetNConstituents_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(1);  
   hClone_RECOPFJetNConstituents_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(2);  
   hClone_HLTPFJetNConstituents_smallestDR_Pt30GeV_EtaL2p4->Draw();
   hClone_RECOPFJetNConstituents_smallestDR_Pt30GeV_EtaL2p4->Draw("sames");
 
-  c10.cd(4);
+  c11.cd(4);
   hClone_HLTPFJetChargedHadronEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(1);  
   hClone_RECOPFJetChargedHadronEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(2);  
   hClone_HLTPFJetChargedHadronEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->Draw();
   hClone_RECOPFJetChargedHadronEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->Draw("sames");
 
-  c10.cd(5);
+  c11.cd(5);
   hClone_HLTPFJetChargedMultiplicity_smallestDR_Pt30GeV_EtaL2p4->GetXaxis()->SetRangeUser(0,100);
   hClone_HLTPFJetChargedMultiplicity_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(1);  
   hClone_RECOPFJetChargedMultiplicity_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(2);  
   hClone_HLTPFJetChargedMultiplicity_smallestDR_Pt30GeV_EtaL2p4->Draw();
   hClone_RECOPFJetChargedMultiplicity_smallestDR_Pt30GeV_EtaL2p4->Draw("sames");
 
-  c10.cd(6);
-  c10.cd(6)->SetLogy();
-  c10.cd(6)->SetLogx();
+  c11.cd(6);
+  c11.cd(6)->SetLogy();
+  c11.cd(6)->SetLogx();
   hClone_HLTPFJetChargedEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->GetYaxis()->SetRangeUser(0.1,hClone_HLTPFJetChargedEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->GetMaximum()*10);
   hClone_HLTPFJetChargedEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(1);  
   hClone_RECOPFJetChargedEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(2);  
   hClone_HLTPFJetChargedEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->Draw();
   hClone_RECOPFJetChargedEmEnergyFraction_smallestDR_Pt30GeV_EtaL2p4->Draw("sames");
 
-  TCanvas c11;
-  c11.Divide(2,1);
+  if(Case==0)
+    c11.SaveAs("DATA_JetID_HLT_vs_RECO.gif");
+  else if(Case==1)
+    c11.SaveAs("MC_JetID_HLT_vs_RECO.gif");
 
-  c11.cd(1);
+  TCanvas c12;
+  c12.Divide(2,1);
+
+  c12.cd(1);
   hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_EtaL2p4->SetLineColor(1);
   hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_EtaL2p4->SetMarkerColor(1);
   hClone_PtBias_reco_hlt_smallestDR_Pt30GeV_EtaL2p4->SetLineColor(2);
@@ -239,16 +299,16 @@
   hClone_PtBias_reco_hlt_smallestDR_Pt30GeV_EtaL2p4->DrawNormalized("sames");
   hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_EtaL2p4->DrawNormalized("sames");
 
-  TLegend legend11_a (0.35,0.65,0.75,0.85);
-  legend11_a.SetFillColor(kWhite);
-  legend11_a.SetMargin(0.2);
-  legend11_a.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_EtaL2p4,"PT>10 GeV","pl");
-  legend11_a.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt30GeV_EtaL2p4,"PT>30 GeV","pl");
-  legend11_a.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt50GeV_EtaL2p4,"PT>50 GeV","pl");
-  legend11_a.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt100GeV_EtaL2p4,"PT>100 GeV","pl");
-  legend11_a.Draw();
+  TLegend legend12_a (0.35,0.65,0.75,0.85);
+  legend12_a.SetFillColor(kWhite);
+  legend12_a.SetMargin(0.2);
+  legend12_a.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_EtaL2p4,"PT>10 GeV","pl");
+  legend12_a.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt30GeV_EtaL2p4,"PT>30 GeV","pl");
+  legend12_a.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt50GeV_EtaL2p4,"PT>50 GeV","pl");
+  legend12_a.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt100GeV_EtaL2p4,"PT>100 GeV","pl");
+  legend12_a.Draw();
 
-  c11.cd(2);
+  c12.cd(2);
   hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_Eta2p4_3->SetLineColor(1);
   hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_Eta2p4_3->SetMarkerColor(1);
   hClone_PtBias_reco_hlt_smallestDR_Pt30GeV_Eta2p4_3->SetLineColor(2);
@@ -263,15 +323,19 @@
   hClone_PtBias_reco_hlt_smallestDR_Pt30GeV_Eta2p4_3->DrawNormalized("sames");
   hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_Eta2p4_3->DrawNormalized("sames");
 
-  TLegend legend11_b (0.35,0.65,0.75,0.85);
-  legend11_b.SetFillColor(kWhite);
-  legend11_b.SetMargin(0.2);
-  legend11_b.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_Eta2p4_3,"PT>10 GeV","pl");
-  legend11_b.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt30GeV_Eta2p4_3,"PT>30 GeV","pl");
-  legend11_b.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt50GeV_Eta2p4_3,"PT>50 GeV","pl");
-  legend11_b.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt100GeV_Eta2p4_3,"PT>100 GeV","pl");
-  legend11_b.Draw();
+  TLegend legend12_b (0.35,0.65,0.75,0.85);
+  legend12_b.SetFillColor(kWhite);
+  legend12_b.SetMargin(0.2);
+  legend12_b.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt10GeV_Eta2p4_3,"PT>10 GeV","pl");
+  legend12_b.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt30GeV_Eta2p4_3,"PT>30 GeV","pl");
+  legend12_b.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt50GeV_Eta2p4_3,"PT>50 GeV","pl");
+  legend12_b.AddEntry(hClone_PtBias_reco_hlt_smallestDR_Pt100GeV_Eta2p4_3,"PT>100 GeV","pl");
+  legend12_b.Draw();
 
+  if(Case==0)
+    c12.SaveAs("DATA_JetPtBias_HLT_vs_RECO.gif");
+  else if(Case==1)
+    c12.SaveAs("MC_JetPtBias_HLT_vs_RECO.gif");
 
 
 }
