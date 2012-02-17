91490a5e9dcf62858df43f0232835f5bad5c27dc
eberry <>
2012-02-17 11:48:59 +0000

    Added options to use sherpa

--- plotting2011/calculateTTBarScale.py
+++ plotting2011/calculateTTBarScale.py
@@ -2,6 +2,49 @@ from ROOT import *
 import os, math, sys
 from optparse import OptionParser
 
+def GetIntegralTH1( histo, xmin, xmax):
+    #get integral
+    axis = histo.GetXaxis()
+    bmin = axis.FindBin(xmin)
+    bmax = axis.FindBin(xmax)
+    bminResidual = histo.GetBinContent(bmin)*(xmin-axis.GetBinLowEdge(bmin)) / axis.GetBinWidth(bmin)
+    bmaxResidual = histo.GetBinContent(bmax)*(axis.GetBinUpEdge(bmax)-xmax) / axis.GetBinWidth(bmax)
+    integral = histo.Integral(bmin,bmax) - bminResidual - bmaxResidual
+    return integral
+
+def GetErrorIntegralTH1( histo, xmin, xmax):
+    print "## calculating error for integral of histo " + str(histo)
+    print "## in the x range [" + str(xmin) + "," + str(xmax) + "]"
+    #get integral
+    axis = histo.GetXaxis()
+    bmin = axis.FindBin(xmin)
+    bmax = axis.FindBin(xmax)
+    bminResidual = histo.GetBinContent(bmin)*(xmin-axis.GetBinLowEdge(bmin)) / axis.GetBinWidth(bmin)
+    bmaxResidual = histo.GetBinContent(bmax)*(axis.GetBinUpEdge(bmax)-xmax) / axis.GetBinWidth(bmax)
+    integral = histo.Integral(bmin,bmax) - bminResidual - bmaxResidual
+    error = 0
+    for bin in range(bmin, bmax+1):
+	print "bin: " +str(bin)
+        if(bin==bmax and bmaxResidual==histo.GetBinContent(bmax)): # skip last bin if out of range
+            print "     --> skip bin: " + str(bin)
+        else:
+            error = error + histo.GetBinError(bin)**2
+            print "error**2 : " + str(error)
+
+    error = sqrt(error)
+    print  " "
+    return error
+
+# def GetIntegralAndError ( histo, xmin, xmax ) : 
+#     integral = GetErrorIntegralTH1( histo, xmin, xmax)
+#     error    = GetIntegralTH1     ( histo, xmin, xmax)
+#     return integral, error
+
+def getIntAndError ( hist ) :
+    error = Double(0.0)
+    integral = hist.IntegralAndError ( 0, hist.GetNbinsX() + 1, error )
+    return integral, error 
+
 usage = "usage: %prog [options] \nExample: python calculateTTBarScale.py -p " + os.environ['LQDATA'] + "/enujj_analysis/enujj/output_cutTable_lq_enujj/analysisClass_lq_enujj_plots.root -q " + os.environ['LQDATA'] + "/enujj_analysis/enujj_qcd/output_cutTable_lq_enujj/analysisClass_lq_enujj_QCD_plots.root -x config/xsection_7TeV_Summer2011.txt -s TTJets_TuneZ2_7TeV-madgraph-tauola -n MTenu_50_110_4jet -l 1.0"
 
 parser = OptionParser(usage=usage)
@@ -30,12 +73,16 @@ parser.add_option("-l", "--qcdScale", dest="qcd_scale",
                   help="Scale factor for QCD histograms",
                   metavar="QCDSCALE")
 
-(options, args) = parser.parse_args()
+parser.add_option("-r", "--useSherpa", dest="use_sherpa",
+                  help="Whether to use SHERPA for W/Z samples (default is 'no')",
+                  metavar="USESHERPA")
 
-def getIntAndError ( hist ) :
-    error = Double(0.0)
-    integral = hist.IntegralAndError ( 0, hist.GetNbinsX() + 1, error )
-    return integral, error 
+parser.add_option("-f","--newXSectionFile", dest="new_file",
+                  help="Name of the new cross section file",
+                  metavar="NEWFILE" )
+
+
+(options, args) = parser.parse_args()
 
 if len ( sys.argv ) < 12 : 
     print usage 
@@ -46,10 +93,19 @@ qcd_file_name          = options.qcd_file_name
 xsection_file_name     = options.xsection_file_name    
 sample_name            = options.sample_name           
 plot_name              = options.plot_name    
+use_sherpa             = options.use_sherpa
 qcd_scale              = float ( options.qcd_scale )
+
+if options.use_sherpa:
+    use_sherpa = options.use_sherpa.lower()
 
 other_background_samples = ["ZJet_Madgraph"     ,"PhotonJets"     ,"SingleTop" ,"DIBOSON"     , "WJet_Madgraph"     ]
 other_background_titles  = ["$Z/Z^{*}$ + jets"  ,"$\gamma$ + jets","single top","WW + WZ + ZZ", "$W/W^{*}$ + jets"  ]
+
+if options.use_sherpa:
+    if use_sherpa == "yes" :
+        other_background_samples = ["ZJet_Sherpa"       ,"PhotonJets"     ,"SingleTop" ,"DIBOSON"     , "WJet_Sherpa"       ]
+        other_background_titles  = ["$Z/Z^{*}$ + jets"  ,"$\gamma$ + jets","single top","WW + WZ + ZZ", "$W/W^{*}$ + jets"  ]
 
 scale_background_sample  = "TTbar_Madgraph"
 scale_background_title   = "$t\\bar{t}$"   
@@ -60,6 +116,9 @@ other_background_hists = []
 other_background_ints  = []
 other_background_eInts  = []
 
+xmin = 40
+xmax = 120
+
 preselection_file = TFile ( preselection_file_name ) 
 qcd_file = TFile ( qcd_file_name ) 
 
@@ -68,10 +127,10 @@ data_int, data_eInt = getIntAndError ( data_hist )
 
 qcd_background_hist = qcd_file.Get("histo1D__DATA__" + plot_name )
 qcd_background_hist.Scale ( qcd_scale ) 
-qcd_background_int, qcd_background_eInt = getIntAndError ( qcd_background_hist ) 
+qcd_background_int, qcd_background_eInt = getIntAndError ( qcd_background_hist )
 
 scale_hist = preselection_file.Get("histo1D__" + scale_background_sample + "__" + plot_name )
-scale_int, scale_eInt = getIntAndError ( scale_hist  )
+scale_int, scale_eInt = getIntAndError ( scale_hist )
 
 other_backgrounds_int = 0.0
 other_backgrounds_eInt = 0.0
@@ -82,7 +141,7 @@ for sample in other_background_samples:
     hist = preselection_file.Get( hist_name ) 
     other_background_hists.append ( hist ) 
 
-    hist_int, hist_eInt = getIntAndError ( hist ) 
+    hist_int, hist_eInt = getIntAndError ( hist )
 
     other_background_ints.append ( hist_int ) 
     other_background_eInts.append ( hist_eInt ) 
@@ -106,6 +165,11 @@ eR = R * math.sqrt ( ( ( eRnum / Rnum ) * ( eRnum / Rnum ) ) +
                      ( ( eRden / Rden ) * ( eRden / Rden ) ) )
 
 latex_file_name = "table_TTBarScale.tex"
+
+if options.use_sherpa:
+    if use_sherpa == "yes" :
+        latex_file_name = "table_TTBarScale_WZSherpa.tex"
+
 latex_file = open ( latex_file_name, "w" )  
 
 latex_file.write("\documentclass{article}\n")
@@ -153,7 +217,10 @@ os.system ("ls -l " + qcd_file_name )
 print "\n\n I calculate: "
 print " R_TTBar = " + "%.3f" % R + " +/- " + "%.3f" % eR 
 
-new_xsection_file_name = xsection_file_name.replace(".txt", "_TTBarScaled.txt")
+if not options.new_file:
+    new_xsection_file_name = xsection_file_name.replace(".txt", "_TTBarScaled.txt")
+else :
+    new_xsection_file_name = options.new_file
 
 xsection_file = open ( xsection_file_name, "r" )
 new_xsection_file = open (new_xsection_file_name, "w" ) 
